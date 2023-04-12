e8b5e346a63fa84676190b705198ec497db6f536
vadym <sinitsavr@gmail.com>
2023-04-12 16:51:27 +0300

    98 add captcha

--- src/api/api.js
+++ src/api/api.js
@@ -1,51 +1,88 @@
-import axios from "axios"
+import axios from "axios";
 
-const instance = axios.create({
-  withCredentials: true,
-  baseURL: `https://social-network.samuraijs.com/api/1.0`,
-  headers: {"API-KEY": "93a8948b-9e2b-4763-af0c-36e8a944067a"}
-});
+const instance = axios.create( {
+   withCredentials: true,
+   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+   headers: {'API-KEY': 'a991bdb8-903c-4ace-9a79-3ab2d68d863e'}
+} )
+
+
+export const authAPI = {
+
+   me() {
+      return instance.get( `auth/me` );
+   },
+
+   login(values) {
+      return instance.post(
+         `auth/login`,
+         values );
+   },
+
+   logout() {
+      return instance.delete( `auth/login` );
+   }
+
+}
+
+
 export const usersAPI = {
-  getUsers(currentPage=1, pageSize=10){
-  return instance.get(
-    `users?page=${currentPage}&count=${pageSize}`).then(response=>{
-    return response.data})
-},
- follow(userId) {
-  return instance.post( 
-  `/follow/${userId}`)},
- unfollow(userId) {
-  return instance.delete(
-  `/follow/${userId}`)}
-,
- getProfile(userId) {
-  // console.warn("Obsolete method. Please profileAPI Object")
-  return profileAPI.getProfile(userId);}}
+
+   getUsers(currentPage, pageSize) {
+      return instance.get( `users?page=${currentPage}&count=${pageSize}` )
+         .then( response => response.data );
+   },
+
+
+   unfollow(userId) {
+      return instance.delete( `follow/${userId}` );
+   },
+
+
+   follow(userId) {
+      return instance.post( `follow/${userId}` );
+   },
+
+   getProfile(userId) {
+      //console.warn('это старый метод - переделай его на
+      //profileAPI.getProfile');
+      return profileAPI.getProfile( userId );
+   }
+
+}
+
 
 export const profileAPI = {
-  getProfile(userId) {
-    return instance.get
-      (`/profile/`+userId);},
-    getStatus(userId){
-        return instance.get(`profile/status/` + userId)
-      },
-      updateStatus(status){
-        return instance.put(`profile/status`,{status:status});
-      },
-      savePhoto(photoFile) {
-        const formData = new FormData();
-        formData.append( 'image', photoFile);
-        return instance.put(`profile/photo`, formData)
-      },
-      saveProfile(formData) {
-        return instance.put(`profile`, formData)
-      }
-    }
-export const  authAPI = {
-  me(){
-      return  instance.get(`/auth/me`)}, 
-      login(email, password, rememberMe = false){
-      return instance.post(`auth/login`, {email, password, rememberMe});
-    },
-    logout() {
-      return instance.delete(`auth/login`)}}
+
+   getProfile(userId) {
+      return instance.get( `profile/` + userId );
+   },
+
+   getStatus(userId) {
+      return instance.get( `profile/status/` + userId );
+   },
+
+   updateStatus(status) {
+      return instance.put( `profile/status`, {status: status} );
+   },
+
+   savePhoto(photoFile) {
+      const formData = new FormData();
+      formData.append( 'image', photoFile );
+      return instance.put( `profile/photo`, formData );
+   },
+
+   saveProfile(formData) {
+      return instance.put( `profile`, formData );
+   }
+
+}
+
+export const securityAPI = {
+
+   getCaptchaAPI() {
+      return instance.get( `/security/get-captcha-url` );
+   },
+
+
+}
--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,138 +1,150 @@
-import React from "react";
-// import {  reduxForm, submit } from 'redux-form';
-import {
-  validateEmailField,
-  ErrorMessageWrapper,
-} from "../../utils/validators";
-import style from "../../utils/ErrorMessage.module.css";
-// import { Input } from '../common/FormsControl/FormControl';
-import { connect } from "react-redux";
-import { login } from "../../redux/authReducer";
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from "formik";
 import * as Yup from "yup";
-import { Formik, Field, ErrorMessage } from "formik";
-import { Navigate } from "react-router-dom";
+import {ErrorMessageWrapper, validateEmailField} from "../../utils/validators";
+import {connect} from "react-redux";
+import {login} from "../../redux/authReducer";
+import {Navigate} from "react-router-dom";
+
+
+
 
 const LoginPage = (props) => {
-  const validationSchema = Yup.object().shape({
-    password: Yup.string()
-      .min(2, "Must be longer than 2 characters")
-      .max(15, "Must be shorter than 15 characters")
-      .required("Required 2"),
-  });
-  if (props.isAuth) {
-    return <Navigate to={"/profile"} />;
-  }
-  return (
-    <div>
-      <h2> Login page </h2>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-          rememberMe: false,
-          general: "",
-        }}
-        validate={validateEmailField}
-        validationSchema={validationSchema}
-        onSubmit={(values, bagWithMethods) => {
-          let { setStatus, setFieldValue, setSubmitting } = bagWithMethods;
-          props.login(
-            values.email,
-            values.password,
-            values.rememberMe,
-            setStatus,
-            setFieldValue,
-            setSubmitting
-          );
-        }}
-      >
-        {(props) => {
-          let { status, values, isSubmitting } = props;
-          
-          return (
-            <form onSubmit={props.handleSubmit}>
-              <div>
-                {values.general && (
-                  <div>_.{values.general} - with setFieldValue</div>
-                )}
-                {status && (
-                  <div className={style.validationErrorMessage}>
-                    ..{status} - with setStatus
-                  </div>
-                )}
-                <div>
-                  <Field name={"email"} type={"text"} placeholder={"e-mail"} />
-                </div>
-                <ErrorMessage name="email">{ErrorMessageWrapper}</ErrorMessage>
-                <div>
-                  <Field
-                    name={"password"}
-                    type={"password"}
-                    placeholder={"password"}
-                  />
-                </div>
-                <ErrorMessage name="password">
-                  {ErrorMessageWrapper}
-                </ErrorMessage>
-                <div>
-                  <Field
-                    type={"checkbox"}
-                    name={"rememberMe"}
-                    id="rememberMe"
-                  />
-                  <label htmlFor={"rememberMe"}> remember me </label>
-                </div>
-                <button type={"submit"} disabled={isSubmitting}>
-                  {isSubmitting ? "Please wait..." : "Submit"}
-                </button>
-              </div>
-            </form>
-          );
-        }}
-      </Formik>
-      <div></div>
-    </div>
-  );
-};
 
-const mapStateToProps = (state) => ({ isAuth: state.auth.isAuth });
+   const validationSchema = Yup.object().shape( {
 
-const LoginPageConnect = connect(mapStateToProps, {login})(LoginPage);
+      password: Yup.string()
+         .min( 2, "Must be longer than 2 characters" )
+         .max( 15, "Must be shorter than 15 characters" )
+         .required( "Required 2" )
+   } );
+
+   if (props.isAuth) {
+      return <Navigate to={'/profile'} />
+   }
+
+   return (
+      <div>
+         <h2> ... Login page </h2>
+
+         <Formik
+            initialValues={{
+               email: '',
+               password: '',
+               rememberMe: false,
+               general: '',
+               captcha: ''
+            }}
+            validate={validateEmailField}
+            validationSchema={validationSchema}
+
+            onSubmit={(values, bagWithMethods) => {
+
+               let {setStatus, setFieldValue, setSubmitting} = bagWithMethods;
+
+               //debugger
+
+               props.login(
+                  values,
+                  setStatus,
+                  setFieldValue,
+                  setSubmitting );
+
+            }}
+         >
+            {(propsF) => {
+
+               let {status, values, isSubmitting} = propsF;
+             
+               return (
+                  <Form>
+
+                     <div>
+
+                        {values.general &&
+                        <div>
+                           _.{values.general} - with setFieldValue
+                        </div>}
+
+                        {status &&
+                        <div>
+                           ..{status}
+                        </div>}
+
+                        {status && props.captchaUrl &&
+                           <div>
+
+                              <div>
+                                 <img src={props.captchaUrl} alt={status}/>
+                              </div>
+
+                              <div>
+                                 <Field
+                                    name={'captcha'}
+                                    type={'text'}/>
+                              </div>
+
+                           </div>
+
+                        }
+
+                        <div>
+                           <Field
+                              name={'email'}
+                              type={'text'}
+                              placeholder={'e-mail'} />
+                        </div>
+                        <ErrorMessage name="email">
+                           {ErrorMessageWrapper}
+                        </ErrorMessage>
+
+                        <div>
+                           <Field
+                              name={'password'}
+                              type={'password'}
+                              placeholder={'password'} />
+                        </div>
+                        <ErrorMessage name="password">
+                           {ErrorMessageWrapper}
+                        </ErrorMessage>
+
+                        <div>
+                           <Field
+                              type={'checkbox'}
+                              name={'rememberMe'}
+                              id='rememberMe' />
+                           <label htmlFor={'rememberMe'}> remember me </label>
+                        </div>
+
+                        <button type={'submit'}
+                                disabled={isSubmitting}
+                        >{isSubmitting ? "Please wait..." : "Submit"}</button>
+
+                     </div>
+
+
+                  </Form>
+               )
+            }
+            }
+         </Formik>
+
+         <div>
+            ...
+         </div>
+
+
+      </div>
+   )
+}
+
+
+const mapStateToProps = (state) => ({
+      isAuth: state.auth.isAuth,
+      captchaUrl: state.auth.captchaUrl
+   }
+);
+
+const LoginPageConnect = connect( mapStateToProps, {login} )( LoginPage );
 
 export default LoginPageConnect;
-
-// const LoginForm = (props) => {
-//   return (
-//       <form onSubmit={props.handleSubmit}>
-//         <div>
-//           <Field name={'login'}
-//           placeholder={'Login'}
-//           validate={[required]}
-//           component={Input}/>
-//         </div>
-//         <div> <Field name={'password'}
-//         placeholder={'Password'}
-//         validate={[required]}
-//         component={Input}
-//         type={"password"}/></div>
-//         <div><Field name={'remember me'}
-//         component={Input}
-//         type={'checkbox'}/> remember me</div>
-//         <button type={'submit'}>Login</button>
-//       </form>
-//   );
-// }
-
-// const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
-
-// const Login = (props)=>{
-//   const onSubmit = (formData) => {
-//     props.login(formData.email, formData.password, formData.rememberMe)
-
-//   }
-//   return <div>
-//     <h1>Login</h1>
-//     <LoginReduxForm onSubmit={onsubmit}/>
-//   </div>
-// }
-// export default connect (null, { login })(Login) ;
